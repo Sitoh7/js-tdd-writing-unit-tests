e43d8fa4f5e11ed94a820a93cbb82fda75ccd614
Sitoh7 <ha.wakhule@gmail.com>
2024-11-07 10:37:20 +0300

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,7 @@
-// Your tests here
+it("handles uppercase and lowercase input", () => {
+    const word = "tEsT";
+  
+    const points = pointsForWord(word);
+  
+    expect(points).toBe(7);
+  });
